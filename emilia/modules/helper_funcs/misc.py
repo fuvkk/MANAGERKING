c0460e3d4cc53cd45fa2c82a7088c35f9151cfbd
CrePavan <93823376+CrePavan@users.noreply.github.com>
2022-03-16 14:18:54 +0530

    Update misc.py

--- emilia/modules/helper_funcs/misc.py
+++ emilia/modules/helper_funcs/misc.py
@@ -39,31 +39,59 @@ def split_message(msg: str) -> List[str]:
         return result
 
 
+
 def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if not chat:
         modules = sorted(
-            [EqInlineKeyboardButton(x.__mod_name__,
-                                    callback_data="{}_module({})".format(prefix, x.__mod_name__.lower())) for x
-             in module_dict.values()])
+            [
+                EqInlineKeyboardButton(
+                    x.__mod_name__,
+                    callback_data="{}_module({})".format(
+                        prefix, x.__mod_name__.lower()
+                    ),
+                )
+                for x in module_dict.values()
+            ]
+        )
     else:
         modules = sorted(
-            [EqInlineKeyboardButton(x.__mod_name__,
-                                    callback_data="{}_module({},{})".format(prefix, chat, x.__mod_name__.lower())) for x
-             in module_dict.values()])
+            [
+                EqInlineKeyboardButton(
+                    x.__mod_name__,
+                    callback_data="{}_module({},{})".format(
+                        prefix, chat, x.__mod_name__.lower()
+                    ),
+                )
+                for x in module_dict.values()
+            ]
+        )
 
-    pairs = list(zip(modules[::2], modules[1::2]))
+    pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
 
-    if len(modules) % 2 == 1:
+    round_num = len(modules) / 3
+    calc = len(modules) - round(round_num)
+    if calc in [1, 2]:
         pairs.append((modules[-1],))
 
-    max_num_pages = ceil(len(pairs) / 5)
+    max_num_pages = ceil(len(pairs) / 3)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-    if len(pairs) > 5:
-        pairs = pairs[modulo_page * 5:5 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("⌫", callback_data="{}_prev({})".format(prefix, modulo_page)),
-             EqInlineKeyboardButton("⌦", callback_data="{}_next({})".format(prefix, modulo_page)))]
+    if len(pairs) > 3:
+        pairs = pairs[modulo_page * 3 : 3 * (modulo_page + 1)] + [
+            (
+                EqInlineKeyboardButton(
+                    "⌫", callback_data="{}_prev({})".format(prefix, modulo_page)
+                ),
+                EqInlineKeyboardButton("ʜᴏᴍᴇ", callback_data="aboutmanu_"),
+                EqInlineKeyboardButton(
+                    "⌦", callback_data="{}_next({})".format(prefix, modulo_page)
+                ),
+            )
+        ]
+
+    else:
+        pairs += [[EqInlineKeyboardButton("ʜᴏᴍᴇ", callback_data="aboutmanu_")]]
 
     return pairs
 
